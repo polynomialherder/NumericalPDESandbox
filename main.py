350ba3bf8de9b6db817129a5f5eb6dcde80864a6
Hugo Saavedra <affinespaces@unm.edu>
2021-06-10 12:06:06 -0600

    Add naive fft_solve function

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import math
 
 import numpy as np
+from scipy.fft import fft, ifft, fftfreq, fftshift
 
 from solver.boundary import BCType, BoundaryCondition
 from solver.second_order import PoissonSolver
 from solver.second_order import _2norm
 
+def fft_solve(solver, n):
+    solver.rows = n
+    # Since the boundary conditions in the setup above are periodic, F(x) = f(x)
+    # for all x in the domain (there are no "correction" terms at the boundaries)
+    transformed = fft(solver.F)
+    shifted = fftshift(transformed)
+    k = -(solver.rows // 2)
+    for idx, _ in enumerate(shifted):
+        if not k:
+            k += 1
+            continue
+        shifted[idx] += (shifted[idx] * solver.upper_bound**2)/(4*np.pi*k**2)
+        k += 1
+    return ifft(shifted)
 
 if __name__ == '__main__':
     f = lambda x: -4*np.sin(2*x)
@@ -14,4 +29,5 @@ if __name__ == '__main__':
     beta = BoundaryCondition(BCType.PERIODIC)
     solver = PoissonSolver(f, rows=10, alpha=alpha, beta=beta, lower_bound=0, upper_bound=2*math.pi, actual=u)
     #solver.plot_h_vs_error()
+    test_solution = fft_solve(solver, 10)
 
