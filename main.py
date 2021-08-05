d7716a0a6b505e586b53b9f2e83ed662c8457b22
Hugo Saavedra <affinespaces@unm.edu>
2021-08-05 07:59:45 -0600

    Add testing code for 2D Poisson solver

--- main.py
+++ main.py
@@ -2,21 +2,40 @@ import math
 
 import numpy as np
 
+from numpy.fft import fft2, fftshift, ifft2
+
 from solver.boundary import BCType, BoundaryCondition
 from solver.second_order import PoissonSolver
 from solver.second_order import _2norm
 
-if __name__ == '__main__':
-    # Use cos for f and u
-    # Make sure that domain is an integer number of periods
-    # Different values of rows
-    f = lambda x: -4*np.sin(2*x)
-    u = lambda x: np.sin(2*x)
-    alpha = BoundaryCondition(BCType.PERIODIC)
-    beta = BoundaryCondition(BCType.PERIODIC)
-    solver = PoissonSolver(
-        f, rows=10, alpha=alpha, beta=beta, lower_bound=0, upper_bound=2*math.pi, actual=u
-    )
-    # Will automatically use fft_solution here, since our BCs are periodic
-    test_solution2 = solver.solution
+def fft_kinv_squared(k):
+        midpoint = k // 2
+        # kinv_squared is a generator with k-values in the right places per Python's fft transformation implementation
+        # For example, given rows = 9, kinv_squared will return a generator containing the
+        # following values:
+        #   [0, (1/1)**2, (1/2)**2, (1/3)**2, (1/4)**2, (-1/4)**2, (-1/3)**2, (-1/2)**2, (-1/1)**2]
+        kinv_squared = (0 if not i else (1/i)**2 if i <= midpoint else 1/(k-i)**2 for i in range(k))
+        return np.fromiter(kinv_squared, dtype=float, count=k)
 
+
+
+if __name__ == '__main__':
+    # This "f" is just a placeholder until I come up with a valid test case
+    f = lambda x, y: -4*np.sin(2*x*y)
+    u = lambda x: np.sin(2*x)
+
+    # Assume a square domain, so upper_x = upper_y
+    # and lower_x = lower_y
+    upper_bound = 2*math.pi
+    lower_bound = 0
+    rows = 10
+    h = (upper_bound - lower_bound)/rows
+    # Create a discretized domain corresponding to the square domain
+    x_ = np.linspace(lower_bound + h/2, upper_bound - h/2, rows, endpoint=True)
+    y_ = np.linspace(lower_bound + h/2, upper_bound - h/2, rows, endpoint=True)
+    x, y = np.meshgrid(x_, y_)
+    F = f(x,y)
+    transformed = fft2(F)
+    kinv_squared = fft_kinv_squared(rows)
+    fourier_coefficients = ((upper_bound - lower_bound)**2)/(4*np.pi**2)
+    U = np.real(ifft2(transformed*fourier_coefficients))
