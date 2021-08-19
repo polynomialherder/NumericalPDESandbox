da038aa8429bc836c6450c854f07135c22ddb1af
Hugo Saavedra <affinespaces@unm.edu>
2021-08-19 09:38:05 -0600

    Add methods for computing 2-norm, non-square arrays

--- main.py
+++ main.py
@@ -2,135 +2,23 @@ import math
 
 import numpy as np
 
-from numpy.fft import fft2, fftshift, ifft2
-
-from solver.boundary import BCType, BoundaryCondition
-from solver.second_order import PoissonSolver
-from solver.second_order import _2norm
-
-class PoissonSolver2D:
-
-    def __init__(self, f, rows, x_lower, x_upper, y_lower, y_upper):
-        self.f = f
-        self.rows = rows
-        self.x_lower = x_lower
-        self.x_upper = x_upper
-        self.y_lower = y_lower
-        self.y_upper = y_upper
-        # Note that x and y are initialized and refreshed when self.F is
-        # invoked
-        self.x = None
-        self.y = None
-
-    @property
-    def length_x(self):
-        return self.x_upper - self.x_lower
-
-    @property
-    def h(self):
-        return self.length_x/self.rows
-
-    @property
-    def length_y(self):
-        return self.y_upper - self.y_lower
-
-    @property
-    def l(self):
-        return self.length_y/self.rows
-
-    @property
-    def _x(self):
-        return np.linspace(self.x_lower + self.h/2, self.x_upper - self.h/2, self.rows, endpoint=True)
-
-    @property
-    def _y(self):
-        return np.linspace(self.y_lower + self.l/2, self.y_upper - self.l/2, self.rows, endpoint=True)
-
-    @property
-    def meshgrid(self):
-        return np.meshgrid(self._x, self._y)
-
-    @property
-    def F(self):
-        self.x, self.y = self.meshgrid
-        return self.f(self.x, self.y)
-
-
-    @property
-    def coefficients(self):
-        """ Compute the Fourier coefficients based on the scipy fft implementation
-
-        At a high level, this method works in the following way:
-        1. The row indices are computed (see the explanation below)
-        2. For each row index in the list of row indices, calculate the row of coefficients.
-           This entails computing i -- which is always equal to the row index -- and j, which
-           is computed in the same manner that the row indices were computed.
-        3. The Fourier coefficient for the i,jth term is then calculated. This coefficient is of
-           the form -1/(d_i + d_j), where d_i is the denominator term that depends on i, and likewise 
-           for d_j.
-        4. Transform the row into an array, and append it to an accumulator
-        5. Return the accumulator as an array
-
-        Note that this method performs some unnecessary extra computation -- see TODO item below
-        """
-        midpoint = self.rows // 2
-        # row_indices is a generator with k-values in the right places per Python's fft
-        # transformation implementation. For example, given rows = 9, row_indices will
-        # be a generator containing following values:
-        #   [0, 1, 2, 3, 4, -4, -3, -2, -1]
-        row_indices = (i if i <= midpoint else (self.rows-i) for i in range(self.rows))
-        denominator_term_x = lambda n: (2*math.pi*n/self.length_x)**2
-        denominator_term_y = lambda n: (2*math.pi*n/self.length_y)**2
-        coefficients = []
-        # TODO: We don't actually need to calculate j for each iteration here, since the value for j
-        #       will be the same for the given column index. We should refactor this so that j is
-        #       computed only once.
-        for row_index in row_indices:
-            row = []
-            for column_index in range(self.rows):
-                if not row_index and not column_index:
-                    row.append(0)
-                    continue
-                elif column_index <= midpoint:
-                    j = column_index
-                else:
-                    j = self.rows - column_index
-                i = row_index
-                denominator = denominator_term_y(i) + denominator_term_x(j)
-                row.append(-1/denominator)
-            coefficients.append(
-                np.fromiter(row, dtype=float)
-            )
-        return np.array(coefficients)
-
-    @property
-    def transformed(self):
-        return fft2(self.F)
-
-    @property
-    def complex_solution(self):
-        return ifft2(self.transformed*self.coefficients)
-
-    @property
-    def U(self):
-        return np.real(self.complex_solution)
-
-    @property
-    def solution(self):
-        """ Synonymous with PoissonSolver2D.U """
-        return self.U
+from solver.poisson_2d import PoissonSolver2D
 
 
 
 if __name__ == '__main__':
-    # This lambda is just placeholder value until I come up with a valid test case
-    f = lambda x, y: -4*np.sin(2*x*y)
+    f = lambda x, y: (12*x**2-24*x+8)*(y**4-4*y**3+4*y**2)+(x**4-4*x**3+4*x**2)*(12*y**2-24*y+8)
+    actual = lambda x, y, area: (x**4-4*x**3+4*x**2)*(y**4-4*y**3+4*y**2) - (256/225)/area
 
     # Initialize a 2D solver
-    p = PoissonSolver2D(f, 10, 0, 1, 0, 25)
+    p = PoissonSolver2D(f, 10, 10, 0, 1, 0, 2, actual)
 
     # Inspect the complex solution (inverse fft2 of fft2(F)*fourier_coefficients prior to coercion to a real-valued array)
     complex_solution = p.complex_solution
 
     # Inspect the real solution
     real_solution = p.U
+
+    f2 = lambda x, y: 2*np.cos(2*x) + 2*np.cos(2*y)
+    actual2 = lambda x, y, area: (np.sin(x))**2 + (np.sin(y))**2-1
+    p2 = PoissonSolver2D(f2, 10, 10, 0, 2*math.pi, 0, 2*math.pi, actual2)
---
+++ solver/poisson_2d.py
@@ -0,0 +1,141 @@
+import math
+
+import numpy as np
+
+from numpy.fft import fft2, ifft2
+
+
+class PoissonSolver2D:
+
+    def __init__(self, f, rows_x, rows_y, x_lower, x_upper, y_lower, y_upper, actual):
+        self.f = f
+        self.rows_x = rows_x
+        self.rows_y = rows_y
+        self.x_lower = x_lower
+        self.x_upper = x_upper
+        self.y_lower = y_lower
+        self.y_upper = y_upper
+        # Note that x and y are initialized and refreshed when self.F is
+        # invoked
+        self.x = None
+        self.y = None
+        self.actual = actual
+
+    @property
+    def length_x(self):
+        return self.x_upper - self.x_lower
+
+    @property
+    def h(self):
+        return self.length_x/self.rows_x
+
+    @property
+    def length_y(self):
+        return self.y_upper - self.y_lower
+
+    @property
+    def l(self):
+        return self.length_y/self.rows_y
+
+    @property
+    def domain_area(self):
+        return self.length_x * self.length_y
+
+    @property
+    def _x(self):
+        return np.linspace(self.x_lower + self.h/2, self.x_upper - self.h/2, self.rows_x, endpoint=True)
+
+    @property
+    def _y(self):
+        return np.linspace(self.y_lower + self.l/2, self.y_upper - self.l/2, self.rows_y, endpoint=True)
+
+    @property
+    def meshgrid(self):
+        return np.meshgrid(self._x, self._y)
+
+    @property
+    def F(self):
+        self.x, self.y = self.meshgrid
+        return self.f(self.x, self.y)
+
+
+    def apply_actual(self):
+        self.x, self.y = self.meshgrid
+        return self.actual(self.x, self.y, self.domain_area)
+
+
+    @property
+    def coefficients(self):
+        """ Compute the Fourier coefficients based on the scipy fft implementation
+
+        At a high level, this method works in the following way:
+        1. The row indices are computed (see the explanation below)
+        2. For each row index in the list of row indices, calculate the row of coefficients.
+           This entails computing i -- which is always equal to the row index -- and j, which
+           is computed in the same manner that the row indices were computed.
+        3. The Fourier coefficient for the i,jth term is then calculated. This coefficient is of
+           the form -1/(d_i + d_j), where d_i is the denominator term that depends on i, and likewise 
+           for d_j.
+        4. Transform the row into an array, and append it to an accumulator
+        5. Return the accumulator as an array
+
+        Note that this method performs some unnecessary extra computation -- see TODO item below
+        """
+        midpoint_x = self.rows_x // 2
+        midpoint_y = self.rows_y // 2
+        # row_indices is a generator with k-values in the right places per Python's fft
+        # transformation implementation. For example, given rows = 9, row_indices will
+        # be a generator containing following values:
+        #   [0, 1, 2, 3, 4, -4, -3, -2, -1]
+        row_indices = (i if i <= midpoint_y else (self.rows_y-i) for i in range(self.rows_y))
+        denominator_term_x = lambda n: (2*math.pi*n/self.length_x)**2
+        denominator_term_y = lambda n: (2*math.pi*n/self.length_y)**2
+        coefficients = []
+        # TODO: We don't actually need to calculate j for each iteration here, since the value for j
+        #       will be the same for the given column index. We should refactor this so that j is
+        #       computed only once.
+        for row_index in row_indices:
+            row = []
+            for column_index in range(self.rows_x):
+                if not row_index and not column_index:
+                    row.append(0)
+                    continue
+                elif column_index <= midpoint_x:
+                    j = column_index
+                else:
+                    j = self.rows_x - column_index
+                i = row_index
+                denominator = denominator_term_y(i) + denominator_term_x(j)
+                row.append(-1/denominator)
+            coefficients.append(
+                np.fromiter(row, dtype=float)
+            )
+        return np.array(coefficients)
+
+    @property
+    def transformed(self):
+        return fft2(self.F)
+
+    @property
+    def complex_solution(self):
+        return ifft2(self.transformed*self.coefficients)
+
+    @property
+    def U(self):
+        return np.real(self.complex_solution)
+
+    @property
+    def solution(self):
+        """ Synonymous with PoissonSolver2D.U """
+        return self.U
+
+    @property
+    def error(self):
+        return abs(self.U - self.apply_actual())
+
+    @property
+    def twonorm(self):
+        return self.pnorm(2)
+
+    def pnorm(self, p):
+        return (sum(sum(self.error**2))*self.h*self.l)**1/p
