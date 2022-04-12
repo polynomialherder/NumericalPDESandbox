7df6f50ef8bc633b0cc96e66916545f253be0683
Hugo Saavedra <affinespaces@unm.edu>
2022-04-12 14:01:16 -0600

    Move immersed boundary solver dependencies into ib_solver module

--- main.py
+++ main.py
@@ -1,135 +1,48 @@
-import math
-
-from math import pi
-
-import numpy as np
-
-from solver.stokes import StokesSolver
+from dataclasses import dataclass
+from math import floor
+from itertools import product
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.rc('text', usetex=True)
-plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
-
-def test_factory(L, H, N):
-    if isinstance(N, float):
-        N = int(N)
-    a = 2
-    b = 2
-    p = lambda x, y: -0*x/L - 0
-    p_x = lambda x, y: -2/L
-    p_y = lambda x, y: 0
-
-    u_actual = lambda x, y: L*np.sin(a*pi*x/L)*np.cos(b*pi*y/H)
-    uxx = lambda x, y: -(a**2 * pi**2 * np.cos(b*pi*y/H)*np.sin(a*pi*x/L))/L
-    uyy = lambda x, y: -(b**2 * L * pi**2* np.cos(b*pi*y/H)*np.sin(a*pi*x/L))/H**2
-
-    v_actual = lambda x, y: -H*np.cos(a*pi*x/L)*np.sin(b*pi*y/H)
-    vyy = lambda x, y: (b**2 * pi**2 * np.cos(a*pi*x/L)*np.sin(b*pi*y/H))/H
-    vxx = lambda x, y: (a**2 * H * pi**2 * np.cos(a*pi*x/L)*np.sin(b*pi*y/H))/L**2
-    f = lambda x, y: uxx(x, y) + uyy(x, y) - p_x(x, y)
-    g = lambda x, y: vxx(x, y) + vyy(x, y) - p_y(x, y)
-    return StokesSolver(
-        f=f,
-        g=g,
-        u_actual=u_actual,
-        v_actual=v_actual,
-        p_actual=p,
-        rows_x=N,
-        rows_y=N,
-        x_lower=0,
-        x_upper=L,
-        y_lower=0,
-        y_upper=H
-    )
+from scipy.linalg import norm
+from solver.stokes import StokesSolver
+from solver.ib_solver import Membrane, Fluid, Simulation
 
 
 if __name__ == '__main__':
-    s1 = test_factory(1, 1, 1000)
-    sqrt_resolutions = np.array([
-        32, 45, 71, 100, 141, 224
-    ])
-    errors_u_p1 = []
-    errors_u_p2 = []
-    errors_u_pinf = []
+    # Define membrane components
+    theta = np.linspace(0, 2 * np.pi, 2000)
+    theta = theta[0:-1]
+    X = np.cos(theta) / 3 + 1 / 2
+    Y = np.sin(theta) / 3 + 1 / 2
 
-    errors_v_p1 = []
-    errors_v_p2 = []
-    errors_v_pinf = []
+    # Define a nonsense force function
+    force = lambda x, y: np.sign(x) + np.sign(y)
+    k = 0.01
 
-    errors_p_p1 = []
-    errors_p_p2 = []
-    errors_p_pinf = []
+    # Define fluid grid
+    L = 1
+    H = 1
+    Nx = 1000
+    Ny = 1000
+    dx = L / Nx
+    dy = H / Ny
+    Xp = np.linspace(dx / 2, L - dx / 2, Nx)
+    Yp = np.linspace(dy / 2, H - dy / 2, Ny)
+    xv, yv = np.meshgrid(Xp, Yp)
 
-    for sqrt_n in sqrt_resolutions:
-        print(f"Calculating errors on grid resolution N={sqrt_n**2:,G}")
-        s = test_factory(1, 1, sqrt_n)
+    # Instantiate membrane and fluid
+    membrane = Membrane(X, Y, k)
+    fluid = Fluid(xv, yv)
+    fluid.register(membrane)
 
-        errors_u_p1.append(
-            s.error_u(p=1)
-        )
-        errors_u_p2.append(
-            s.error_u(p=2)
-        )
-        errors_u_pinf.append(
-            s.error_u(p=np.Inf)
-        )
+    dt = 0.001
+    simulation = Simulation(fluid, membrane, dt)
 
-        errors_v_p1.append(
-            s.error_v(p=1)
-        )
-        errors_v_p2.append(
-            s.error_v(p=2)
-        )
-        errors_v_pinf.append(
-            s.error_p(p=np.Inf)
-        )
-
-        errors_p_p1.append(
-            s.error_p(p=1)
-        )
-        errors_p_p2.append(
-            s.error_p(p=2)
-        )
-        errors_p_pinf.append(
-            s.error_p(p=np.Inf)
-        )
-
-    p1_color = "forestgreen"
-    p2_color = "slateblue"
-    pinf_color = "maroon"
-
-    fig, ax = plt.subplots()
-
-    resolutions = sqrt_resolutions**2
-    ax.set_title("$\\lVert \\hat{u} - u \\rVert$")
-    ax.plot(resolutions, errors_u_p1, "--o", color=p1_color, label="$1$-norm")
-    ax.plot(resolutions, errors_u_p2, "--o", color=p2_color, label="$2$-norm")
-    ax.plot(resolutions, errors_u_pinf, "--o", color=pinf_color, label=r"$\infty$-norm")
-    ax.set_xscale("log", base=10)
-    ax.set_yscale("log", base=2)
-    ax.grid()
-    fig.legend()
-    fig.show()
-
-    fig, ax = plt.subplots()
-    ax.set_title("$\\lVert \\hat{v} - v \\rVert$")
-    ax.plot(resolutions, errors_v_p1, "--o", color=p1_color, label="$1$-norm")
-    ax.plot(resolutions, errors_v_p2, "--o", color=p2_color, label="$2$-norm")
-    ax.plot(resolutions, errors_v_pinf, "--o", color=pinf_color, label=r"$\infty$-norm")
-    ax.set_xscale("log", base=10)
-    ax.set_yscale("log", base=2)
-    ax.grid()
-    fig.legend()
-    fig.show()
-
-    fig, ax = plt.subplots()
-    ax.set_title("$\\lVert \\hat{p} - p \\rVert$")
-    ax.plot(resolutions, errors_p_p1, "--o", color=p1_color, label="$1$-norm")
-    ax.plot(resolutions, errors_p_p2, "--o", color=p2_color, label="$2$-norm")
-    ax.plot(resolutions, errors_p_pinf, "--o", color=pinf_color, label=r"$\infty$-norm")
-    ax.set_xscale("log", base=10)
-    ax.set_yscale("log", base=2)
-    ax.grid()
-    fig.legend()
-    fig.show()
+    t_end = 0.05
+    t = 0.0
+    while t < t_end:
+        print(f"{t=}")
+        simulation.step().plot()
+        t += dt
--- solver/ib_solver.py
+++ solver/ib_solver.py
@@ -1,161 +1,257 @@
-import random
-
-from functools import cached_property
+from dataclasses import dataclass
+from math import floor
+from itertools import product
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+from scipy.linalg import norm
 from solver.stokes import StokesSolver
 
-class ImmersedBoundary:
 
-    def __init__(self, x, y):
-        self.x_raw = x
-        self.y_raw = y
-        self.fluid = None
+def delta_spread(r, h):
+    return (1 / h) * (1 + np.cos(np.pi * r / (2 * h))) / 4
 
-    @property
-    def zipped(self):
-        return np.array(list(zip(self.x_raw, self.y_raw)), dtype=[('x', float), ('y', float)])
+@dataclass
+class SimulationStep:
 
+    Fx: np.array
+    Fy: np.array
+    fx: np.array
+    fy: np.array
+    u: np.array
+    v: np.array
+    p: np.array
+    U: np.array
+    V: np.array
+    X: np.array
+    Y: np.array
+    t: float
 
-    @property
-    def x(self):
-        return self.zipped['x']
-
-    @property
-    def y(self):
-        return self.zipped['y']
-
-    @property
-    def ds_left(self):
-        xkm1 = np.roll(self.x, -1)
-        ykm1 = np.roll(self.y, -1)
-        return np.sqrt((xkm1 - self.x)**2 + (ykm1 - self.y)**2)
-
-    @property
-    def ds_right(self):
-        xkp1 = np.roll(self.x, -1)
-        ykp1 = np.roll(self.y, -1)
-        return np.sqrt((xkp1 - self.x)**2 + (ykp1 - self.y)**2)
-
-    @property
-    def dS(self):
-        return (self.ds_left + self.ds_right)/2
-
-    @staticmethod
-    def delta_spread(r, h):
-        if abs(r) < 2:
-            return 0
-        return (1/h)*(1 + np.cos(np.pi*r/(2*h)))
+    def plot(self):
+        plt.quiver(self.X, self.Y, self.Fx, self.Fy)
+        plt.title(f"t={self.t}")
+        plt.draw()
+        plt.pause(0.1)
 
 
-    def spread(self, fn):
-        dx = self.fluid.dx
-        dy = self.fluid.dy
-        F = fn(self.fluid.xv, self.fluid.yv)
-        f = np.zeros(self.fluid.xv.shape)
-        for xk, yk, ds in zip(self.x, self.y, self.dS):
-            mask = np.logical_and(
-                abs(self.fluid.xv - xk) < 2*self.fluid.dx,
-                abs(self.fluid.yv - yk) < 2*self.fluid.dy
-            )
-            diff_x = (self.fluid.xv - xk)[mask]
-            diff_y = (self.fluid.yv - yk)[mask]
-            f[mask] += self.delta_spread(diff_x, self.fluid.dx) * self.delta_spread(diff_y, self.fluiddy) * ds
-        return F*f
+class Simulation:
 
-
+    def __init__(self, fluid, membrane, dt, t=0, mu=1):
+        self.fluid = fluid
+        self.membrane = membrane
+        self.dt = dt
+        self.mu = mu
+        self.t = t
 
     def calculate_forces(self):
-        pass
+        return self.membrane.Fx, self.membrane.Fy
 
+    def spread_forces(self, Fx, Fy):
+        return self.fluid.spread(Fx), self.fluid.spread(Fy)
 
-    def update_positions(self, new_x, new_y):
-       # This is a little awkward -- I think in practice we might use actual getter
-        # and setter methods here, which would enable us to cache the properties properly
-        # rather than dynamically calculating them each time by zipping x and y
-        self.x_raw = new_x
-        self.y_raw = new_y
+    def stokes_solve(self, fx, fy):
+        return self.fluid.stokes_solve(fx, fy)
+
+    def calculate_velocities(self, u, v):
+        return self.membrane.interp(u), self.membrane.interp(v)
+
+    def update_membrane_positions(self, U, V):
+        self.membrane.X += self.dt*U
+        self.membrane.Y += self.dt*V
+
+    def step(self):
+        Fx, Fy = self.calculate_forces()
+        fx, fy = self.spread_forces(Fx, Fy)
+        u, v, p = self.stokes_solve(fx, fy)
+        U, V = self.calculate_velocities(fx, fy)
+        self.update_membrane_positions(U, V)
+        self.t += self.dt
+        return SimulationStep(Fx, Fy, fx, fy, u, v, p, U, V, self.membrane.X, self.membrane.Y, self.t)
 
 
 class Fluid:
 
-    def __init__(self, X, Y):
-        self.t = 0
+    def __init__(self, xv, yv, mu=1, membrane=None):
+        self.xv = xv
+        self.yv = yv
+        self.membrane = membrane
+        self.solver = StokesSolver(self.xv, self.yv, mu=mu)
+
+    def register(self, membrane):
+        self.membrane = membrane
+        self.membrane.fluid = self
+
+    def stokes_solve(self, fx, fy):
+        self.solver.F = fx
+        self.solver.G = fy
+        return self.solver.u, self.solver.v, self.solver.p
+
+    @property
+    def shape(self):
+        return self.xv.shape
+
+    def spread(self, F):
+
+        dx = 1 / self.xv[0].size
+        dy = 1 / len(self.yv)
+        xlen, ylen = self.xv.shape
+        f = np.zeros((xlen, ylen))
+        for xk, yk, ds, Fk in zip(self.membrane.X, self.membrane.Y, self.membrane.dS, F):
+            xk_dx = (xk - dx / 2) / dx
+            yk_dy = (yk - dy / 2) / dy
+            floored_x = floor(xk_dx)
+            floored_y = floor(yk_dy)
+            # If floored_x == xk_dx, then floored_x - 1 will only
+            # capture 1 gridpoint to the left of xk rather than 2
+            # Likewise for floored_y and yk_dy
+            correction_term_x = -1 if floored_x == xk_dx else 0
+            correction_term_y = -1 if floored_y == yk_dy else 0
+            x_range = range(
+                floored_x - 1 + correction_term_x, floored_x + 3 + correction_term_x
+            )
+            y_range = range(
+                floored_y - 1 + correction_term_y, floored_y + 3 + correction_term_y
+            )
+            i = floored_x - 1 + correction_term_x
+            for i, j in product(x_range, y_range):
+                jm = j % ylen
+                im = i % xlen
+                Xk = self.xv[jm, im]
+                Yk = self.yv[jm, im]
+                f[jm, im] += Fk * delta_spread(Xk - xk, dx) * delta_spread(Yk - yk, dy) * ds
+        return f
+
+
+
+class Membrane:
+
+    def __init__(self, X, Y, k, fluid=None, p=2):
         self.X = X
         self.Y = Y
-        self._xv = None
-        self._yv = None
-        self.dx = 1/self.X.size
-        self.dy = 1/self.Y.size
+        self.k = k
+        self.p = p
+        self.fluid = fluid
 
-    def register(self, immersed_boundary: ImmersedBoundary):
-        self.immersed_boundary = immersed_boundary
-        immersed_boundary.fluid = self
+
+    def interp(self, f):
+        if self.fluid is None:
+            helpful_message = """Membrane has not been registered to any fluid; cannot interp
+            Create a Fluid object and call Fluid.register(membrane) before interpolating forces
+            """
+            raise Exception(helpful_message)
+        dx = 1 / self.fluid.xv[0].size
+        dy = 1 / len(self.fluid.yv)
+        F = np.zeros(self.X.shape)
+        zipped = zip(self.X, self.Y)
+        xlen, ylen = self.fluid.shape
+        for membrane_idx, xk_yk in enumerate(zipped):
+            xk, yk = xk_yk
+            xk_dx = (xk - dx / 2) / dx
+            yk_dy = (yk - dy / 2) / dy
+            floored_x = floor(xk_dx)
+            floored_y = floor(yk_dy)
+            # If floored_x == xk_dx, then floored_x - 1 will only
+            # capture 1 gridpoint to the left of xk rather than 2
+            # Likewise for floored_y and yk_dy
+            correction_term_x = -1 if floored_x == xk_dx else 0
+            correction_term_y = -1 if floored_y == yk_dy else 0
+            x_range = range(
+                floored_x - 1 + correction_term_x, floored_x + 3 + correction_term_x
+            )
+            y_range = range(
+                floored_y - 1 + correction_term_y, floored_y + 3 + correction_term_y
+            )
+            i = floored_x - 1 + correction_term_x
+            for i, j in product(x_range, y_range):
+                jm = j % ylen
+                im = i % xlen
+                Xk = self.fluid.xv[jm, im]
+                Yk = self.fluid.yv[jm, im]
+                F[membrane_idx] += (
+                    f[jm, im]
+                    * delta_spread(Xk - xk, dx)
+                    * delta_spread(Yk - yk, dy)
+                    * dx
+                    * dy
+                )
+        return F
+
+
+
+    def difference_minus(self, vec):
+        shifted = np.roll(vec, -1)
+        return shifted - vec
+
+    def difference_plus(self, vec):
+        shifted = np.roll(vec, 1)
+        return shifted - vec
 
     @property
-    def meshgrid(self):
-        return np.meshgrid(self.X, self.Y)
+    def difference_minus_x(self):
+        return self.difference_minus(self.X)
 
     @property
-    def xv(self):
-        if self._xv is None:
-            self._xv, self._yv = self.meshgrid
-        return self._xv
+    def difference_plus_x(self):
+        return self.difference_plus(self.X)
+
 
     @property
-    def yv(self):
-        if self._yv is None:
-            self._xv, self._yv = self.meshgrid
-        return self._yv
+    def norm_minus_x(self):
+        return norm(self.difference_minus_x, self.p)
 
+    @property
+    def norm_plus_x(self):
+        return norm(self.difference_minus_x, self.p)
 
-    def step_position(self, x, y):
-        return x + np.random.uniform(0, 0.5, x.shape)*(-1)**self.t, y + np.random.uniform(0, 0.5, y.shape)*(-1)**self.t
+    @property
+    def tau_minus_x(self):
+        return self.difference_minus_x / self.dS_minus
 
+    @property
+    def tau_plus_x(self):
+        return self.difference_plus_x / self.dS_plus
 
-    def update_immersed_boundary_position(self):
-        # This might be more appropriate as a method belonging to the Immersed_Boundary class
-        # The Immersed_Boundary constructor might instead take a position_fn parameter which
-        # is called on each time step rather than being called by the IBSolver
-        new_x, new_y = self.step_position(
-            self.immersed_boundary.x,
-            self.immersed_boundary.y
-        )
-        self.immersed_boundary.update_positions(new_x, new_y)
-        self.t += 1
+    @property
+    def difference_minus_y(self):
+        return self.difference_minus(self.Y)
 
+    @property
+    def difference_plus_y(self):
+        return self.difference_plus(self.Y)
 
-    def plot(self):
-        fig, ax = plt.subplots()
-        ax.plot(self.immersed_boundary.x, self.immersed_boundary.y, '--o')
-        ax.set_xlim(-5, 10)
-        ax.set_ylim(-5, 10)
-        ax.grid()
-        ax.set_title(f"t={self.t}")
-        fig.savefig(f"Plots/{self.t:03}.png")
+    @property
+    def tau_minus_y(self):
+        return self.difference_minus_y / self.dS_minus
 
+    @property
+    def tau_plus_y(self):
+        return self.difference_plus_y / self.dS_plus
 
-if __name__ == '__main__':
-    membrane_X = np.array(
-        [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 0.9, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.1, 0.1]
-    )
-    membrane_Y = np.array(
-        [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.3, 0.6, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.6, 0.3]
-    )
-    m = ImmersedBoundary(
-        membrane_X, membrane_Y
-    )
+    @property
+    def tau_x(self):
+        return self.tau_minus_x + self.tau_plus_x
 
-    X = np.linspace(0, 1, 1000)
-    Y = np.linspace(0, 1, 1000)
-    fluid = Fluid(X, Y)
-    fluid.register(m)
+    @property
+    def tau_y(self):
+        return self.tau_minus_y + self.tau_plus_y
 
-    force = lambda x, y: np.sign(x) + np.sign(y)
-    f = m.spread(force)
-    plt.plot(membrane_X, membrane_Y, "ro")
-    plt.pcolor(fluid.xv, fluid.yv, f)
-    plt.colorbar()
-    plt.show()
+    @property
+    def dS_minus(self):
+        return np.sqrt(self.difference_minus_x**2 + self.difference_minus_y**2)
+
+    @property
+    def dS_plus(self):
+        return np.sqrt(self.difference_plus_x**2 + self.difference_plus_y**2)
+
+    @property
+    def dS(self):
+        return (self.dS_minus + self.dS_plus)/2
+
+    @property
+    def Fx(self):
+        return self.k*(self.tau_plus_x - self.tau_minus_x)/self.dS
+
+    @property
+    def Fy(self):
+        return self.k*(self.tau_plus_y - self.tau_minus_y)/self.dS
