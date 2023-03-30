2981d24f1cc605bf984921075cf5b556111fafe3
Hugo Saavedra <affinespaces@unm.edu>
2023-03-30 16:25:03 -0600

    Add sandbox for PyDistMesh hello world

--- sandbox.py
+++ sandbox.py
@@ -1,22 +1,35 @@
+import distmesh as dm
 import matplotlib.pyplot as plt
 import numpy as np
 
-from numpy import pi
 
-if __name__ == "__main__":
-    L = 5
-    H = 5
-    n = 35
-    X = np.linspace(0, L, n)
-    Y = np.linspace(0, H, n)
-    xv, yv = np.meshgrid(X, Y)
-    x_coef = 4.73
-    y_coef = 5
-    u = L * np.sin(x_coef * pi * xv / L) * np.cos(y_coef * pi * yv / H)
-    v = -H * np.cos(x_coef * pi * xv / L) * np.sin(y_coef * pi * yv / H)
-    plt.contourf(xv, yv, u)
-    plt.show()
-    plt.contourf(xv, yv, v)
-    plt.show()
-    plt.quiver(xv, yv, u, v)
-    plt.show()
+def meshplot(p, t, write_location=None):
+    fig, ax = plt.subplots()
+    # PyDistMesh appears to access this deprecated property
+    # from matplotlib.Axes, so we initialize it to keep it from
+    # throwing an exception
+    ax._hold = True
+    dm.plotting.axes_simpplot2d(ax, p, t)
+    if not write_location:
+        fig.show()
+    else:
+        fig.savefig(write_location)
+
+
+def circle_mesh(r: float, q: np.ndarray, h=0.2):
+    def distance(x):
+        return np.sqrt(((x - q)**2).sum(1)) - r
+
+    bbox = (q[0] - r, q[1] - r, q[0] + r, q[1] + r)
+    return dm.distmesh2d(distance, dm.huniform, h, bbox)
+
+
+if __name__ == '__main__':
+    points, connections = circle_mesh(2.5, np.array([3, 4]))
+    meshplot(points, connections)
+
+    # Poor man's meshplot
+    fig, ax = plt.subplots()
+    ax.plot(points[:, 0], points[:, 1], "o")
+    ax.grid()
+    fig.show()
