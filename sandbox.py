bd01b5e54a31c05b736f454f4a57bb7a7b00ff40
Hugo Saavedra <hm@topologi.es>
2023-07-24 13:01:30 -0600

    Add compression and shear tests

--- sandbox.py
+++ sandbox.py
@@ -9,13 +9,6 @@ from numpy.linalg import inv
 from solver.fluid import Fluid
 from solver.ib_utils import spread, interp
 
-"""
-Structured vs unstructured
-Make the force 1
-Spread it to the fluid grid
-Should get a little plateau (approximately 1 in the place where there is mesh, and drops off to 0 in the place where there is not mesh)
-"""
-
 MU = 1
 K = 1
 
@@ -70,8 +63,12 @@ class Mesh:
         self.coordinates = coordinates
         self._edges = edges
         self._ref = None
-        self.faces_seen = set()
-        self.faces = []
+        self.faces_seen = {}
+
+
+    @property
+    def faces(self):
+        return list(self.faces_seen.values())
 
 
     @property
@@ -89,7 +86,7 @@ class Mesh:
         return np.array([p.area() for p in self.points])
 
 
-    @cached_property
+    @property
     def adjacencies(self):
         return adjacencies(self._edges)
 
@@ -99,8 +96,6 @@ class Mesh:
         points = []
         for point in self.adjacencies:
             point_index = point
-            coordinates = self.coordinates[point]
-            adjacencies = self.adjacencies[point]
             points.append(
                 Point(
                     point_index, self
@@ -108,6 +103,11 @@ class Mesh:
             )
         points = sorted(points, key=lambda z: z.index)
         return np.array(points)
+    
+
+    def face_points(self, indices):
+        one, two, three = indices
+        return [self.points[one], self.points[two], self.points[three]]
 
 
     def area(self):
@@ -125,10 +125,10 @@ class Mesh:
     def register_face(self, indices):
         indices = self.standardize(indices)
         if indices not in self.faces_seen:
-            self.faces.append(
-                Face(indices, self)
-            )
-            self.faces_seen.add(indices)
+            face = Face(indices, self)
+            self.faces_seen[indices] = face
+            return face
+        return self.faces_seen[indices]
 
 
     def calculate_point_forces(self):
@@ -138,17 +138,33 @@ class Mesh:
 
     @property
     def forces(self):
-        return [np.array(p.force) for p in self.points]
+        return np.array([np.array(p.force) for p in self.points])
     
 
     @property
     def forcesX(self):
-        return [force[0] for force in self.forces]
+        return np.array([force[0] for force in self.forces])
     
     @property
     def forcesY(self):
-        return [force[1] for force in self.forces]
+        return np.array([force[1] for force in self.forces])
+
+
+    @property
+    def reference_x(self):
+        return np.array([face.reference_coordinates[0] for face in self.faces])
+
+
+    @property
+    def reference_y(self):
+        return np.array([face.reference_coordinates[1] for face in self.faces])
+
     
+
+    def initialize(self):
+        for point in self.points:
+            point.find_faces()
+
 
 
 class Face:
@@ -156,50 +172,52 @@ class Face:
     def __init__(self, indices, mesh):
         self.indices = indices
         self.mesh = mesh
-        self._ref = None
-        self.points = [self.mesh.points[index] for index in self.indices]
+        self._points = None
+        self._ref = self.calculate_coordinates()
+
+
+    @property
+    def points(self):
+        return self.mesh.face_points(self.indices)
+    
+
+    @points.setter
+    def points(self, value):
+        self.points = value
 
 
     def calculate_coordinates(self):
-        coordinates = []
-        for index in self.indices:
-            coordinates.append(
-                self.mesh.points[index].coordinates
-            )
-        # coordinates[:] returns a copy of coordinates
-        # simply assigning ref to coordinates would result
-        # in a pointer to the same object
-        self._ref = [np.array(c, copy=True) for c in coordinates]
-        return coordinates
+        return [np.copy(point.coordinates) for point in self.points]
 
 
-    @cached_property
+    @property
     def coordinates(self):
         return self.calculate_coordinates()
 
 
-    @cached_property
+    @property
     def reference_coordinates(self):
-        if self._ref is None:
-            self.calculate_coordinates()
         return self._ref
 
 
-    @cached_property
-    def area(self):
+    def calculate_area(self):
         return calculate_area(*self.coordinates)
 
+
+    def area(self):
+        return self.calculate_area()
+    
 
     def __repr__(self):
         return f"<Face with vertices {self.coordinates}>"
 
 
-    @cached_property
+    @property
     def x1(self):
         return self.coordinates[1] - self.coordinates[0]
 
 
-    @cached_property
+    @property
     def x2(self):
         return self.coordinates[2] - self.coordinates[0]
 
@@ -214,7 +232,7 @@ class Face:
         return self.reference_coordinates[2] - self.reference_coordinates[0]
 
 
-    @cached_property
+    @property
     def s(self):
         return np.column_stack([self.s1, self.s2])
 
@@ -236,7 +254,7 @@ class Face:
 
     @property
     def J(self):
-        return self.delta
+        return np.linalg.det(self.A)
 
 
     @property
@@ -271,7 +289,7 @@ class Face:
     @property
     def dAdx0_1(self):
         return (1/self.delta)*np.array([
-            [self.s1[1] - self.s2[1], -self.s2[0] - self.s1[0]],
+            [self.s1[1] - self.s2[1], self.s2[0] - self.s1[0]],
             [0, 0]
         ])
 
@@ -280,7 +298,7 @@ class Face:
     def dAdx0_2(self):
         return (1/self.delta)*np.array([
             [0, 0],
-            [self.s1[1] - self.s2[1], -self.s2[0] - self.s1[0]]
+            [self.s1[1] - self.s2[1], self.s2[0] - self.s1[0]]
         ])
 
 
@@ -309,18 +327,19 @@ class Face:
     def dWdA(self):
         invAT = inv(self.A.T)
         term_1 = MU*(self.A/self.J - np.trace(self.A*self.A.T)*invAT/(2*self.J))
-        term_2 = K*(self.J - 1)*invAT
+        term_2 = K*(self.J - 1)*self.J*invAT
         return term_1 + term_2
     
 
     def calculate_point_forces(self):
         dWdA = self.dWdA
         for k in (0, 1, 2):
-            force = 0
             for ell in (1, 2):
-                force += (dWdA*self.dAdx(k, ell)).sum()
-                force *= -self.area
-                self.points[k].force[ell-1] += force
+                component_force = 0
+                component_force += (dWdA*self.dAdx(k, ell)).sum()
+                component_force *= -1/self.area() #self.points[k].area()
+                self.points[k].force[ell-1] += component_force
+                
 
 
 class Point:
@@ -330,6 +349,7 @@ class Point:
         self.mesh = parent_mesh
         self._coordinates = None
         self.force = [0, 0]
+        self._faces = []
 
 
     def add_force(self, ell, force_value):
@@ -344,40 +364,53 @@ class Point:
     def y(self):
         return self.coordinates[1]
 
-    @cached_property
+    @property
     def coordinates(self):
         return self.mesh.coordinates[self.index]
+    
+
+    @property
+    def setter(self, other):
+        self.mesh.coordinates[self.index] = other
+
+    
+
+    @property
+    def coordinates_tuple(self):
+        return tuple(self.coordinates)
 
 
-    @cached_property
+    @property
     def adjacencies(self):
         return self.mesh.adjacencies[self.index]
 
 
-    @cached_property
-    def faces(self):
+    def find_faces(self):
         faces = []
         for adjacency in self.adjacencies:
             adjacency_adjacencies = self.mesh.adjacencies[adjacency]
             common = adjacency_adjacencies.intersection(self.adjacencies)
             if common:
                 # There should be one node in common per adjacency
-                face = (self.index, adjacency, common.pop())
-                self.mesh.register_face(face)
-                faces.append(
+                face_indices = (self.index, adjacency, common.pop())
+                face = self.mesh.register_face(face_indices)
+                self._faces.append(
                     face
                 )
         return faces
+    
+
+    @property
+    def faces(self):
+        if not self._faces:
+            self._faces = self.find_faces()
+        return self._faces
 
 
     def area(self):
         area = 0
         for face in self.faces:
-            coordinates = []
-            for idx in face:
-                coordinates.append(self.mesh.points[idx].coordinates)
-            face_area = calculate_area(*coordinates)
-            area += (1/3)*face_area
+            area += (1/3)*face.area()
         return area
 
 
@@ -397,6 +430,103 @@ class Point:
 
     def __repr__(self):
         return f"<Point at {self.coordinates}>"
+    
+
+
+def compression_test(mesh):
+    fig, ax = plt.subplots()
+    coordinates = [p.coordinates for p in mesh.points]
+    foo = np.array(coordinates)
+    ax.plot(foo[:,0],foo[:,1],'or')
+
+
+    for point in mesh.points:
+        shift = np.array([0.5, 0.5])
+        mesh.coordinates[point.index] -= shift
+        mesh.coordinates[point.index][0] *= 0.9
+        mesh.coordinates[point.index] += shift
+
+    mesh.calculate_point_forces()
+
+
+    X = np.linspace(0, 1, 1000)
+    Y = np.linspace(0, 1, 1000)
+    xv, yv = np.meshgrid(X, Y)
+    coordinates = [p.coordinates for p in mesh.points]
+    foo = np.array(coordinates)
+    bar = np.array(mesh.forces)
+    ax.plot(foo[:,0],foo[:,1],'ob')
+    ax.quiver(foo[:,0],foo[:,1], bar[:,0],bar[:,1])
+    fig.show()
+
+
+def shear_test(mesh):
+    fig, ax = plt.subplots()
+    coordinates = [p.coordinates for p in mesh.points]
+    foo = np.array(coordinates)
+    ax.plot(foo[:,0],foo[:,1],'or')
+
+
+    for point in mesh.points:
+        shift = np.array([0.5, 0.5])
+        mesh.coordinates[point.index] -= shift
+        mesh.coordinates[point.index][0] += mesh.coordinates[point.index][0] + 2*mesh.coordinates[point.index][1]
+        mesh.coordinates[point.index] += shift
+
+    mesh.calculate_point_forces()
+
+
+    X = np.linspace(0, 1, 1000)
+    Y = np.linspace(0, 1, 1000)
+    xv, yv = np.meshgrid(X, Y)
+    coordinates = [p.coordinates for p in mesh.points]
+    foo = np.array(coordinates)
+    bar = np.array(mesh.forces)
+    ax.plot(foo[:,0],foo[:,1],'ob')
+    ax.quiver(foo[:,0],foo[:,1], bar[:,0],bar[:,1])
+    fig.show()
+
+
+def spread_test(mesh):
+    for point in mesh.points:
+        shift = np.array([0.5, 0.5])
+        mesh.coordinates[point.index] -= shift
+        mesh.coordinates[point.index][0] += mesh.coordinates[point.index][0] + 2*mesh.coordinates[point.index][1]
+        mesh.coordinates[point.index] += shift
+
+    mesh.calculate_point_forces()
+    X = np.linspace(0, 1, 55)
+    Y = np.linspace(0, 1, 55)
+    xv, yv = np.meshgrid(X, Y)
+    spread_force = spread(
+        np.sqrt((mesh.forces[:,0])**2 + (mesh.forces[:,1])**2),
+        xv,
+        yv,
+        mesh.X,
+        mesh.Y,
+        mesh.dA
+    )
+    fig, ax = plt.subplots()
+    X = np.linspace(0, 1, 55)
+    Y = np.linspace(0, 1, 55)
+    xv, yv = np.meshgrid(X, Y)
+    coordinates = [p.coordinates for p in mesh.points]
+    foo = np.array(coordinates)
+    bar = np.array(mesh.forces)
+    ax.plot(foo[:,0],foo[:,1],'ob')
+    ax.quiver(X,Y, spread_force[:,0],spread_force[:,1])
+    fig.show()
+
+
+def run_tests():
+    print("Building mesh")
+    points, edges = circle_mesh(0.4, np.array([0.5, 0.5]), h=0.1)
+    print("Points and edges determined, building mesh object")
+    for test in [compression_test, shear_test, spread_test]:
+        mesh = Mesh(points, edges)
+        mesh.initialize()
+        test(mesh)
+
 
 
 if __name__ == '__main__':
@@ -404,20 +534,33 @@ if __name__ == '__main__':
     points, edges = circle_mesh(0.4, np.array([0.5, 0.5]), h=0.1)
     print("Points and edges determined, building mesh object")
     mesh = Mesh(points, edges)
+    mesh.initialize()
+
     for point in mesh.points:
         shift = np.array([0.5, 0.5])
-        point.coordinates -= shift
-        point.coordinates *= 1.1
-        point.coordinates += shift
+        mesh.coordinates[point.index] -= shift
+        mesh.coordinates[point.index][0] += mesh.coordinates[point.index][0] + 2*mesh.coordinates[point.index][1]
+        mesh.coordinates[point.index] += shift
 
-    mesh.dA
     mesh.calculate_point_forces()
-
+    X = np.linspace(0, 1, 55)
+    Y = np.linspace(0, 1, 55)
+    xv, yv = np.meshgrid(X, Y)
+    spread_force = spread(
+        np.sqrt((mesh.forces[:,0])**2 + (mesh.forces[:,1])**2),
+        xv,
+        yv,
+        mesh.X,
+        mesh.Y,
+        mesh.dA
+    )
     fig, ax = plt.subplots()
-    X = np.linspace(0, 1, 1000)
-    Y = np.linspace(0, 1, 1000)
+    X = np.linspace(0, 1, 55)
+    Y = np.linspace(0, 1, 55)
     xv, yv = np.meshgrid(X, Y)
     coordinates = [p.coordinates for p in mesh.points]
-    ax.plot(coordinates)
-    ax.quiver(coordinates, mesh.forces)
+    foo = np.array(coordinates)
+    bar = np.array(mesh.forces)
+    ax.plot(foo[:,0],foo[:,1],'ob')
+    ax.quiver(X,Y, spread_force[:,0],spread_force[:,1])
     fig.show()
